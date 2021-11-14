bb74141f950ec82742ba8a61d72b07bb8255d33c
Maxim Silkov <maxim.silkou.2002@gmail.com>
2021-11-14 22:50:24 +0300

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,33 @@
+﻿using Logging.Interface;
+using NLog;
+using System;
+
+namespace ConsoleApp9
+{
+    public class Calculator : IAlgorithm
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public int Calculate(int a, int b)
+        {
+            int res = 0;
+
+            logger.Info($"Lets devide {a} to {b}");
+            try
+            {
+                res = a /= b;
+                logger.Info($"Divison was successfull result = {res}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                logger.Error(ex.Message);
+            }
+            catch
+            {
+                logger.Error("Unknown message");
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Interface/IAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Logging.Interface
+{
+    /// <summary>      
+    /// Provides templates for some algorithm calculating.      
+    /// </summary>      
+    public interface IAlgorithm
+    {
+        int Calculate(int first, int second);
+
+        int Calculate(out long miliseconds, int first, int second)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int res = Calculate(first, second);
+            stopwatch.Stop();
+            miliseconds = stopwatch.ElapsedMilliseconds;
+            return res;
+        }
+    }
+}
